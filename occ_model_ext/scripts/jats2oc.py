b886c3254821f0fb5a9a058788f2156079e14371
marilena daquino <marilena.daquino2@unibo.it>
2019-07-31 19:13:37 +0100

    sentence extraction

--- occ_model_ext/scripts/jats2oc.py
+++ occ_model_ext/scripts/jats2oc.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from lxml import etree as ET
-import pprint , uuid, itertools
-
+import pprint , uuid, itertools , os
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 # TODO 
 # wrapper for iterating over all the xml files
 # control whther the citation style is always the same
 # new function for sentence tokenization - add sentence xpath in fullMetadata
+
+def get_text_before(element):
+    for item in element.xpath("preceding-sibling::*/text()|preceding-sibling::text()"):
+        item = item.strip()
+        if item:
+            yield item
+
+
+def get_text_after(element):
+    for item in element.xpath("following-sibling::*/text()|following-sibling::text()"):
+        item = item.strip()
+        if item:
+            yield item
+
 
 def find_bibRef(xref, root):
 	""" given a xml element xref or the text value of the element xref
@@ -35,6 +49,43 @@ def find_bibRef(xref, root):
 			bibRefUID = 'not found'
 	return bibRefUID
 
+
+def xpathSubstring(xref,root):
+	"""given a xml element and the root element of the xml file, 
+	returns the XPath function identifying the sentence wherein it appears
+	"""
+
+	xrefValue = ET.tostring(xref, method="text", encoding='unicode', with_tail=False)
+	xrefToString = ET.tostring(xref, encoding='unicode', with_tail=False)
+	
+	# enrich the list of abbreviations with Abbreviations.txt
+	ABBREVIATIONS_LIST = ['dr', 'vs', 'mr', 'mrs', 'prof', 'inc', 'al', 'eg', 'fig', 'figs', 'Fig', 'tab', 'eq', 'ref', 'e.g', 'refs', 'i.e', 'cf', 'eqs', 'ca', 'm.f']
+
+	abbpath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Abbreviations.txt'))
+	with open(abbpath, 'r') as f:
+		for line in f.readlines():
+			if not len(line) == 0:
+				ABBREVIATIONS_LIST.append(line.strip())
+
+	# Initialize Sentence Splitter
+	punkt_param = PunktParameters()
+	punkt_param.abbrev_types = set(ABBREVIATIONS_LIST)
+	sentence_splitter = PunktSentenceTokenizer(punkt_param)
+
+	string_before = "".join(get_text_before(xref)) 
+	string_after = "".join(get_text_after(xref))
+
+	# get start offset of the last sentence in the string before the pointer (0-based index transformed in 1-based index to comply with XPath)
+	startSent = int([start for start, end in sentence_splitter.span_tokenize( string_before )][-1])+1  
+	print(sentence_splitter.tokenize( string_before )[-1].encode('utf-8'),sentence_splitter.span_tokenize( string_before )[-1])
+	# get the length of the string
+	strin = sentence_splitter.tokenize( string_before )[-1]+xrefValue+sentence_splitter.tokenize( string_after )[0]
+	print(strin.encode('utf-8'))
+	lenSent = len(strin)
+
+	# create the XPath
+	sentXPathFunction = 'substring(string(./'+ET.ElementTree(root).getpath(xref.getparent())+'),'+str(startSent)+','+str(lenSent)+')'
+	return sentXPathFunction
 
 def extract_intext_ref(filePMC):
 	""" preprocess a nxml file and stores a number of context components in a JSON file, namely:
@@ -67,14 +118,17 @@ def extract_intext_ref(filePMC):
 			group.append('sequence')
 		else:
 			group.append('singleton')
-	print('groups',groups)
 	
 	# remove separators ',' and '\u2013'
 	groupsClean = [list(i for i in j if i not in prepositions) for j in groups]
-	print('groupsClean',groupsClean)
 
 	# include all the metadata related to the in-text references
 	fullMetadata = [[{'xrefElemXPath':'./'+et.getpath(xref), 'xrefValue':xref.text, 'xrefParentElem':xref.getparent().tag, 'xrefParentElemXPath':'./'+et.getpath(xref.getparent()), 'bibRefUID':find_bibRef(xref,root)} if isinstance(xref, str) == False else xref for xref in xrefGroup] for xrefGroup in groupsClean]
+
+	for xrefGroup in groupsClean:
+		for xref in xrefGroup:
+			if isinstance(xref, str) == False:
+				print(xpathSubstring(xref,root))
 
 	# extend sequences
 	for groups in fullMetadata: # lists in list
@@ -86,8 +140,7 @@ def extract_intext_ref(filePMC):
 			rangeValues.sort()
 			for intermediate in range(int(rangeValues[0])+1,int(rangeValues[1])):
 				groups.append({'xrefElemXPath': 'none', 'xrefValue':str(intermediate), 'xrefParentElem':groups[0]['xrefParentElem'], 'xrefParentElemXPath': groups[0]['xrefParentElemXPath'], 'bibRefUID':find_bibRef(str(intermediate),root) })
-		
-	pp.pprint(fullMetadata)
 	
+
 filePMC = 'xml_PMC_sample/PMC5906705.nxml'
 extract_intext_ref(filePMC)
